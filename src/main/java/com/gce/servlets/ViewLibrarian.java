b19a7898ed034f01ca3d15bbed930b5d97839c66
Gayatripani <gayatripani70@gmail.com>
2023-08-25 17:17:55 +0530

    First Commit
    E_LibraryManagementSystem

---
+++ src/main/java/com/gce/servlets/ViewLibrarian.java
@@ -0,0 +1,50 @@
+package com.gce.servlets;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.gce.beans.LibrarianBean;
+import com.gce.dao.LibrarianDao;
+@WebServlet("/ViewLibrarian")
+public class ViewLibrarian extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.print("<!DOCTYPE html>");
+		out.print("<html>");
+		out.println("<head>");
+		out.println("<title>View Librarian</title>");
+		out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+		out.println("<link rel='stylesheet' href='css/style.css'/>");
+		out.println("</head>");
+		out.println("<body>");
+		
+		request.getRequestDispatcher("navadmin.html").include(request, response);
+		out.println("<div class='container'>");
+		
+		List<LibrarianBean> list=LibrarianDao.view();
+		
+		out.println("<table class='table'>");
+		out.println("<tr><th>Id</th><th>Name</th><th>Email</th><th>Password</th><th>Mobile</th><th>Edit</th><th>Delete</th></tr>");
+		for(LibrarianBean bean:list){
+			out.println("<tr><td>"+bean.getId()+"</td><td>"+bean.getName()+"</td><td>"+bean.getEmail()+"</td><td>"+bean.getPassword()+"</td><td>"+bean.getMobile()+"</td><td><a href='EditLibrarianForm?id="+bean.getId()+"'><button type=\"button\" class=\"btn btn-warning\">Edit</button>\r\n"
+					+ "</a></td><td><a href='DeleteLibrarian?id="+bean.getId()+"'><button type=\"button\" class=\"btn btn-danger\">Delete</button>\r\n"
+							+ "</a></td></tr>");
+		}
+		out.println("</table>");
+		
+		out.println("</div>");
+		request.getRequestDispatcher("footer.html").include(request, response);
+		out.close();
+		
+	}
+}
